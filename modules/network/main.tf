68241994821b3d975aed689a3719c065800b97d4
root <root@ezops.local>
2024-01-09 10:43:50 +0000

    chore: adjust network nomeclature

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -14,8 +14,8 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
+  private_subnets = [for key, value in local.azs : cidrsubnet(var.vpc_cidr, 4, key)]
+  public_subnets  = [for key, value in local.azs : cidrsubnet(var.vpc_cidr, 8, key + 48)]
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
